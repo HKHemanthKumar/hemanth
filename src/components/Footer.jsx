6d44fd46616e224a0a559d0b9de1531e24caf4cf
Hemanth <76157667+HemanthKumarHK1806@users.noreply.github.com>
2025-05-09 17:19:28 +0530

    Added user Profile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,12 +12,12 @@ const Footer = () => {
       <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[100px] flex justify-between items-center flex-wrap gap-5`}>
         <div>
           <p className='text-white font-bold text-[24px]'>Hemanth Kumar HK</p>
-          <p className='mt-1 text-secondary'>Infrastructure Engineer</p>
+          <p className='mt-1 text-secondary'>AWS & DevOps Engineer</p>
         </div>
         
         <div className='flex gap-5'>
           <a
-            href="https://github.com/nihharikadubey"
+            href="https://github.com/HKHemanthKumar"
             target="_blank"
             rel="noopener noreferrer"
             className='transition-colors text-secondary hover:text-white'
@@ -25,7 +25,7 @@ const Footer = () => {
             GitHub
           </a>
           <a
-            href="https://linkedin.com/in/nihharikadubey"
+            href="https://www.linkedin.com/in/hk-hemanth-kumar"
             target="_blank"
             rel="noopener noreferrer"
             className='transition-colors text-secondary hover:text-white'
@@ -43,7 +43,7 @@ const Footer = () => {
       
       <div className='flex justify-center items-center py-5 w-full'>
         <p className='text-secondary text-[14px]'>
-          © {new Date().getFullYear()} Niharika Dubey. All rights reserved.
+          © {new Date().getFullYear()} Hemanth Kumar HK. All rights reserved.
         </p>
       </div>
     </motion.footer>
